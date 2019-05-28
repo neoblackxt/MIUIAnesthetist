a3bd077e2d53c34abd6b487f7468c3fa0ff83fc2
neoblackxt <neoblackxt@outlook.com>
2019-05-29 02:14:13 +0800

    init commit

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # MIUIAnesthetist
 MIUI麻醉师Xposed模块，麻醉MIUI，以便对小米应用商店等癌组织进行无痛切除
 
-## 功能
+## 功能特性
 禁止MIUI系统启动时检查自身完整性，防止在删除小米应用商店后无法开机进入系统。理论上支持其他的开机自检的系统应用，如系统更新等。
+禁止MIUI系统拦截冻结部分系统应用操作、禁用组件操作。（全球上网必须在MConnService(VsimCore.apk)冻结之后才能成功冻结）
+禁止MIUI系统将小米应用商店自动设置为默认应用商店劫持Google Play应用商店。~~~（其实就是去你大爷的小米应用商店）~~~ 如无效请恢复默认应用设置： 安全中心->应用管理->右上竖排三点->默认应用设置->恢复默认 鸣谢：跟悟空扯关系@酷安网coolapk.com [去除MIUI强制调用小米应用商店（非改build）](https://www.coolapk.com/feed/8492730?shareKey=MjM2ODkyMTI5Zjg4NWNlZDJhMzI~)、ccat3z@github 我不要小米应用市场
 
 ## 食用方法
-利用magisk的挂载功能，在`/sbin/.magisk/modules/你的模块名/system/app/MiuiSuperMarket/`下建立一个`.replace`文件占位即可，每次开机自动移除垃圾系统应用。
-需要恢复系统应用时，删除或重命名`.replace`文件。
-你可能需要在`/sbin/.magisk/modules/你的模块名`路径下补充模块的说明文件，可参照其他模块修改。或者直接利用已有的模块，比如magisk自带hosts模块（在 magisk设置->Systemless hosts 处开启）
-移除其他系统应用方法同理。
+安装并启用此Xposed模块，重启系统。
+如果需要移除系统应用建议使用release中的Magisk模块，一旦出现异常情况，用mm管理器禁用此magisk模块即可恢复。
+此模块默认只移除MiuiSuperMarket。修改此模块中的install.sh文件，将你想要移除的应用的所在目录填写在REPLACE=""中，一行一个，格式参照上面几行的例子。
+更多信息：[Magisk模块指南](https://topjohnwu.github.io/Magisk/guides.html)
 
 ## 注意事项
-禁用此模块 或 禁用Xposed框架 或 禁用Xposed框架依附的magisk，都会导致此模块功能失效。所以如果你直接删掉了`/system/app/MiuiSuperMarket/MiuiSuperMarket.apk`而不是用magisk进行“伪删除”，并且因误操作致使此模块被禁用，手机会无法开机进入系统。
+禁用此Xposed模块 或 禁用Xposed框架 或 禁用Xposed框架依附的magisk 或 重新安装/版本升级此模块后，xposed installer没检测到此模块更新（此时可能需手动关闭再打开才能再次启用），都会导致此模块功能失效。所以如果你没有使用Magisk而是直接删掉了`/system/app/MiuiSuperMarket/MiuiSuperMarket.apk`，并且万一因误操作致使此模块被禁用，手机会无法开机进入系统。
 
 ## 救援
-将[小米应用商店](http://apkpure.co/xiaomi-market-com-xiaomi-market/)文件放到`/data/app/xiaomimarket/xiaomimarket.apk`,或者刷机（不必清除/data分区）。
+如果因冻结系统应用而无法开机，只能删除`/data/system/users/0/package-restrictions.xml`文件后重启系统。可以在TWRP recovery的 高级->文件管理器 中操作。或在recovery终端中执行命令`rm /data/system/users/0/package-restrictions.xml`。或者用电脑连接recovery模式中的手机，在cmd中执行`adb shell`，然后`rm /data/system/users/0/package-restrictions.xml`。刷机不会有任何帮助，擦除/data分区是可行的但没必要，因为那样会丢失除了/sdcard之外的所有数据。
+如果因删除小米应用商店而无法开机，将[小米应用商店](http://apkpure.co/xiaomi-market-com-xiaomi-market/)文件放到`/data/app/xiaomimarket/xiaomimarket.apk`,或者刷机（不必清除/data分区）。
+
+## 授权与免责
+详见LICENSE
--- app/src/main/java/com/xposed/miuianesthetist/DisableSecurityCheck.java
+++ app/src/main/java/com/xposed/miuianesthetist/DisableSecurityCheck.java
@@ -1,25 +1,151 @@
 package com.xposed.miuianesthetist;
 
+import java.lang.reflect.Field;
+import java.util.List;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
+import android.content.Intent;
+
 public class DisableSecurityCheck implements IXposedHookLoadPackage {
+    private static final Object lock = new Object();
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (lpparam.packageName.equals("android")) {
             try {
-                /**com.miui.server.SecurityManagerService中的checkSystemSelfProtection方法过短，
+                /* 禁用开机检查自身完整性
+                 com.miui.server.SecurityManagerService中的checkSystemSelfProtection方法过短，
                  无法插入跳转指令进行hook，故hook其方法内创建的匿名内部类的run方法 */
                 findAndHookMethod("com.miui.server.SecurityManagerService$1"
                         , lpparam.classLoader, "run", XC_MethodReplacement.DO_NOTHING);
+            } catch (Throwable t) {
+                XposedBridge.log(t);
+            }
+            try {
+                /* 禁用检查应用是否可以被禁用 */
+                findAndHookMethod("com.android.server.pm.PackageManagerServiceInjector"
+                        , lpparam.classLoader, "isAllowedDisable",
+                        String.class, int.class, XC_MethodReplacement.returnConstant(true));
+            } catch (Throwable t) {
+                XposedBridge.log(t);
+            }
+
+            /* 弃用
+            try {
+                *//* 禁用小米应用商店劫持其他应用商店 来自 ccat3z@github [我不要小米应用市场] *//*
+                findAndHookMethod("com.android.server.pm.PackageManagerServiceInjector"
+                        , lpparam.classLoader, "getMarketResolveInfo",
+                        List.class, XC_MethodReplacement.returnConstant(null));
+            } catch (Throwable t) {
+                XposedBridge.log(t);
+            }*/
+
+            try {
+                /* 禁用小米应用商店劫持其他应用商店 来自 跟悟空扯关系@酷安网coolapk.com
+                去除MIUI强制调用小米应用商店（非改build）
+                https://www.coolapk.com/feed/8492730?shareKey=MjM2ODkyMTI5Zjg4NWNlZDJhMzI~ */
+                Field IS_INTERNATIONAL_BUILD = XposedHelpers.findField(XposedHelpers.findClass("miui.os.Build", lpparam.classLoader), "IS_INTERNATIONAL_BUILD");
+                boolean iib = IS_INTERNATIONAL_BUILD.getBoolean(null);
+                if (!iib) {
+                    XposedBridge.log("Get MIUI Build: NOT international build");
+                    Class<?> pmsClazz = XposedHelpers.findClass("com.android.server.pm.PackageManagerService", lpparam.classLoader);
+                    findAndHookMethod("com.android.server.pm.PackageManagerServiceInjector"
+                            , lpparam.classLoader, "checkMiuiIntent",
+                            pmsClazz, Intent.class, String.class, int.class, List.class, int.class, new XC_MethodReplacement() {
+                                @Override
+                                protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                                    Object result;
+                                    synchronized (lock){
+                                        IS_INTERNATIONAL_BUILD.setBoolean(null,true);
+                                        XposedBridge.log("Set MIUI Build to international build");
+                                        result = XposedBridge.invokeOriginalMethod(param.method, param.thisObject, param.args);
+                                        IS_INTERNATIONAL_BUILD.setBoolean(null,false);
+                                        XposedBridge.log("Set MIUI Build back to NOT international build");
+                                    }
+                                    return result;
+                                }
+                            });
+                }
 
             } catch (Throwable t) {
                 XposedBridge.log(t);
             }
+
+            /*
+            static ResolveInfo checkMiuiIntent(PackageManagerService pms, Intent intent, String resolvedType, int flags, List<ResolveInfo> query, int userId) {
+                ResolveInfo ri;
+                if (intent != null && !Build.IS_INTERNATIONAL_BUILD) {
+                    if (intent.getCategories() == null || !intent.getCategories().contains("android.intent.category.HOME")) {
+                        if ("android.intent.action.ASSIST".equals(intent.getAction()) || "android.intent.action.VOICE_COMMAND".equals(intent.getAction())) {
+                            return getSystemResolveInfo(pms, query);
+                        }
+                        if (("http".equals(intent.getScheme()) || "https".equals(intent.getScheme())) && "android.intent.action.VIEW".equals(intent.getAction())) {
+                            ri = UrlResolver.checkMiuiIntent(pms.mContext, pms, intent, resolvedType, flags, query, userId);
+                            if (ri != null) {
+                                return ri;
+                            }
+                            if (VERSION.SDK_INT < 23 && intent.getComponent() == null && intent.getType() == null) {
+                                ri = getBrowserResolveInfo(pms, query);
+                                if (ri != null) {
+                                    return ri;
+                                }
+                            }
+                        } else if ("mimarket".equals(intent.getScheme()) || ("market".equals(intent.getScheme()) && "android.intent.action.VIEW".equals(intent.getAction()))) {
+                            Uri uri = intent.getData();
+                            if (uri != null) {
+                                String host = uri.getHost();
+                                if (host != null && (host.equals("details") || host.equals("search"))) {
+                                    ResolveInfo ri2 = getMarketResolveInfo(query);
+                                    if (ri2 != null) {
+                                        return ri2;
+                                    }
+                                }
+                            }
+                        } else if (PACKAGE_MIME_TYPE.equals(intent.getType()) && "android.intent.action.VIEW".equals(intent.getAction())) {
+                            String realPkgName;
+                            if (Build.IS_INTERNATIONAL_BUILD || AppOpsUtils.isXOptMode() || Build.IS_TABLET) {
+                                realPkgName = PACKAGE_INSTALLER_NAME;
+                                synchronized (pms.mPackages) {
+                                    if (pms.mSettings.getRenamedPackageLPr(PACKAGE_INSTALLER_NAME) != null) {
+                                        realPkgName = pms.mSettings.getRenamedPackageLPr(PACKAGE_INSTALLER_NAME);
+                                    }
+                                }
+                            } else {
+                                realPkgName = MIUI_INSTALLER_PACKAGE;
+                            }
+                            intent.setPackage(realPkgName);
+                            return pms.resolveIntent(intent, resolvedType, flags, userId);
+                        }
+                    } else if (Build.getUserMode() == 1) {
+                        intent.setClassName("com.jeejen.family.miui", "com.jeejen.home.launcher.Launcher");
+                    } else {
+                        intent.setClassName("com.miui.home", "com.miui.home.launcher.Launcher");
+                    }
+                    if (intent.getComponent() != null) {
+                        return pms.resolveIntent(intent, resolvedType, flags, userId);
+                    }
+                } else if (!(intent == null || !Build.IS_INTERNATIONAL_BUILD || AppOpsUtils.isXOptMode())) {
+                    if ((PACKAGE_MIME_TYPE.equals(intent.getType()) && "android.intent.action.VIEW".equals(intent.getAction())) || "android.intent.action.INSTALL_PACKAGE".equals(intent.getAction())) {
+                        intent.setPackage(PACKAGE_INSTALLER_NAME);
+                        return pms.resolveIntent(intent, resolvedType, flags, userId);
+                    } else if (isOpenByGooglePlayStore(intent, pms)) {
+                        ri = getGlobalMarketResolveInfo(query);
+                        if (ri != null) {
+                            return ri;
+                        }
+                    }
+                }
+                return pms.mResolveInfo;
+            }
+
+
+             */
         }
     }
 }
