45b55e1708a03854f3a684c9f533ce667e1c9095
neoblackxt <neoblackxt@outlook.com>
2019-07-10 02:06:12 +0800

    Allow users disable system apps using SecurityCenter

--- app/src/main/java/com/xposed/miuianesthetist/DisableSecurityCheck.java
+++ app/src/main/java/com/xposed/miuianesthetist/DisableSecurityCheck.java
@@ -6,7 +6,12 @@ import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.List;
 
@@ -27,31 +32,34 @@ import static de.robv.android.xposed.XposedHelpers.setBooleanField;
 public class DisableSecurityCheck implements IXposedHookLoadPackage {
 
     private static Boolean iib = null;
+    private static volatile MenuItem menu2 = null;
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
         if (lpparam.packageName.equals("android")) {
 
-            /* Disable integrity check when boot in services.jar */
+            // Disable integrity check when boot in services.jar
             try {
                 Class sms = findClass("com.miui.server.SecurityManagerService"
                         , lpparam.classLoader);
-                findAndHookConstructor(sms, Context.class, boolean.class/*onlyCore*/, new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        // Set the instance of SecurityManagerService class to be onlyCore mode, then checkSystemSelfProtection() method will do nothing
-                        param.args[1] = true;
-                        // Set system apps' states to be not cracked
-                        findField(sms, "mSysAppCracked").setInt(param.thisObject, 0);
-                    }
-                });
+                findAndHookConstructor(sms, Context.class, boolean.class/*onlyCore*/,
+                        new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                                // Set the instance of SecurityManagerService class to be onlyCore mode,
+                                // then checkSystemSelfProtection() method will do nothing
+                                param.args[1] = true;
+                                // Set system apps' states to be not cracked
+                                findField(sms, "mSysAppCracked").setInt(param.thisObject, 0);
+                            }
+                        });
             } catch (Throwable t) {
                 log("miuianesthetist err:");
                 log(t);
             }
 
 
-            /* Remove the limit for disabling system apps in services.jar */
+            // Remove the limit for disabling system apps in services.jar
             try {
                 findAndHookMethod("com.android.server.pm.PackageManagerServiceInjector"
                         , lpparam.classLoader, "isAllowedDisable",
@@ -61,27 +69,33 @@ public class DisableSecurityCheck implements IXposedHookLoadPackage {
                 log(t);
             }
 
-             /* Prevent some ultra sticky system apps (MiuiDaemon, FindDevice, etc.) from running after have been disabled in services.jar */
-            try{
-                findAndHookMethod("com.android.server.am.ActivityManagerServiceInjector", lpparam.classLoader, "shouldAddPersistApp", ApplicationInfo.class,new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws IllegalAccessException {
-                        ApplicationInfo info = (ApplicationInfo)param.args[0];
-                        Object GREEN_KID_AGENT_PKG_NAME = findField(findClass("com.miui.server.GreenGuardManagerService", lpparam.classLoader), "GREEN_KID_AGENT_PKG_NAME").get(null);
-                        if (!TextUtils.equals(info.packageName, (CharSequence) GREEN_KID_AGENT_PKG_NAME) && info.enabled) {
-                            param.setResult(true);
-                            return;
-                        }
-                        String str = "yManagerServiceInjector";//Shorten TAG ActivityManagerServiceInjector
-                        String sb = "persist app : " +
-                                info.packageName +
-                                "should not add to start";
-                        Log.d(str, sb);
-                        param.setResult(false);
-                    }
+            // Prevent some ultra sticky system apps (MiuiDaemon, FindDevice, etc.) from running
+            // after have been disabled in services.jar
+            try {
+                findAndHookMethod("com.android.server.am.ActivityManagerServiceInjector",
+                        lpparam.classLoader, "shouldAddPersistApp", ApplicationInfo.class,
+                        new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                                ApplicationInfo info = (ApplicationInfo) param.args[0];
+                                CharSequence GREEN_KID_AGENT_PKG_NAME = (CharSequence) findField(findClass(
+                                        "com.miui.server.GreenGuardManagerService",
+                                        lpparam.classLoader), "GREEN_KID_AGENT_PKG_NAME").get(null);
+                                if (!TextUtils.equals(info.packageName, GREEN_KID_AGENT_PKG_NAME) &&
+                                        info.enabled) {
+                                    param.setResult(true);
+                                    return;
+                                }
+                                String str = "AMSInjector"; //Shorten TAG ActivityManagerServiceInjector
+                                String sb = "persist app : " +
+                                        info.packageName +
+                                        " should not add to start";
+                                Log.d(str, sb);
+                                param.setResult(false);
+                            }
 
-                });
-            }catch (Throwable t) {
+                        });
+            } catch (Throwable t) {
                 log("miuianesthetist err:");
                 log(t);
             }
@@ -98,22 +112,26 @@ public class DisableSecurityCheck implements IXposedHookLoadPackage {
                 }
             }
 
-            /* Return an intent before it get processed to avoid being hijacked in services.jar */
+            // Return an intent before it get processed to avoid being hijacked in services.jar
             antiHijack:
             {
                 if (iib) {
                     break antiHijack;
                 }
                 try {
-                    Class<?> pmsClazz = findClass("com.android.server.pm.PackageManagerService", lpparam.classLoader);
+                    Class<?> pmsClazz = findClass("com.android.server.pm.PackageManagerService",
+                            lpparam.classLoader);
                     findAndHookMethod("com.android.server.pm.PackageManagerServiceInjector"
                             , lpparam.classLoader, "checkMiuiIntent",
-                            pmsClazz, Intent.class, String.class, int.class, List.class, int.class, new XC_MethodReplacement() {
+                            pmsClazz, Intent.class, String.class, int.class, List.class, int.class,
+                            new XC_MethodReplacement() {
                                 @Override
                                 protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
                                     Intent intent = (Intent) param.args[1];
-                                    if (("market".equals(intent.getScheme()) && "android.intent.action.VIEW".equals(intent.getAction())) ||
-                                            ("http".equals(intent.getScheme()) || "https".equals(intent.getScheme())) &&
+                                    if (("market".equals(intent.getScheme()) &&
+                                            "android.intent.action.VIEW".equals(intent.getAction())) ||
+                                            ("http".equals(intent.getScheme()) ||
+                                                    "https".equals(intent.getScheme())) &&
                                                     "android.intent.action.VIEW".equals(intent.getAction())) {
                                         //return mResolveInfo of the first arg pms
                                         return findField(pmsClazz, "mResolveInfo").get(param.args[0]);
@@ -130,48 +148,76 @@ public class DisableSecurityCheck implements IXposedHookLoadPackage {
 
         //SecurityCenter.apk
         if (lpparam.packageName.equals("com.miui.securitycenter")) {
-            /* Allow user disable system apps WLan network access permission */
-            try {
-                Class<?> ada = findClass("com.miui.appmanager.ApplicationsDetailsActivity", lpparam.classLoader);
-                Method[] declaredMethods = ada.getDeclaredMethods();
-                for (Method m : declaredMethods) {
-                    hookMethod(m, new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) {
-                            try {
-                                if (param.thisObject != null)
-                                    setBooleanField(param.thisObject, "mIsSystem", false);
-                            } catch (Throwable t) {
-                                log("miuianesthetist err:");
-                                log(t);
-                            }
-                        }
-                    });
-                }
 
-            } catch (Throwable t) {
-                log("miuianesthetist err:");
-                log(t);
-            }
-
-            /* Allow user set third-party launcher to be default */
+            // Allow users disable system apps using SecurityCenter
+            // TODO abstract this three method
             try {
-                findAndHookMethod("com.miui.securitycenter.provider.ThirdDesktopProvider", lpparam.classLoader, "call", String.class, String.class, Bundle.class, new XC_MethodHook() {
+                Class<?> ada = findClass("com.miui.appmanager.ApplicationsDetailsActivity",
+                        lpparam.classLoader);
+                findAndHookMethod(ada, "onCreateOptionsMenu", Menu.class, new XC_MethodHook() {
                     @Override
-                    protected void afterHookedMethod(MethodHookParam param) {
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                         try {
-                            if (param.args[0].equals("getModeAndList")) {
-                                Bundle result = (Bundle) param.getResult();
-                                //mode=0 can't use unauthorized third-party launcher; mode=1 can't use official launcher; other value no limit
-                                result.putInt("mode", -1);
-                                param.setResult(result);
-                            }
+                            menu2 = ((Menu) param.args[0]).getItem(1);
+                            menu2.setEnabled(true);
                         } catch (Throwable t) {
                             log("miuianesthetist err:");
                             log(t);
                         }
                     }
                 });
+
+                findAndHookMethod(ada, "onPrepareOptionsMenu", Menu.class, new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        try {
+                            if (menu2 != null)
+                                menu2.setEnabled(true);
+                        } catch (Throwable t) {
+                            log("miuianesthetist err:");
+                            log(t);
+                        }
+                    }
+                });
+
+                findAndHookMethod(ada, "onResume", new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        try {
+                            if (menu2 != null)
+                                menu2.setEnabled(true);
+                        } catch (Throwable t) {
+                            log("miuianesthetist err:");
+                            log(t);
+                        }
+                    }
+                });
+            } catch (Throwable t) {
+                log("miuianesthetist err:");
+                log(t);
+            }
+
+            // Allow users set third-party launcher to be default
+            try {
+                findAndHookMethod("com.miui.securitycenter.provider.ThirdDesktopProvider",
+                        lpparam.classLoader, "call", String.class, String.class,
+                        Bundle.class, new XC_MethodHook() {
+                            @Override
+                            protected void afterHookedMethod(MethodHookParam param) {
+                                try {
+                                    if (param.args[0].equals("getModeAndList")) {
+                                        Bundle result = (Bundle) param.getResult();
+                                        // mode=0 can't use unauthorized third-party launcher;
+                                        // mode=1 can't use official launcher; other value no limit
+                                        result.putInt("mode", -1);
+                                        param.setResult(result);
+                                    }
+                                } catch (Throwable t) {
+                                    log("miuianesthetist err:");
+                                    log(t);
+                                }
+                            }
+                        });
             } catch (Throwable t) {
                 log("miuianesthetist err:");
                 log(t);
